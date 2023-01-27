387f1503d7ca41f58fd60ca516c3741ce9ed8bbe
Gregory Heskett <gheskett@gmail.com>
2023-01-27 02:42:46 -0500

    Add temporary sequence for easy replacement convenience

--- src/audio/external.c
+++ src/audio/external.c
@@ -315,6 +315,7 @@ u8 sBackgroundMusicDefaultVolume[] = {
     70,  // SEQ_EVENT_CUTSCENE_ENDING
     65,  // SEQ_MENU_FILE_SELECT
     0,   // SEQ_EVENT_CUTSCENE_LAKITU (not in JP)
+    127, // SEQ_TEMPORARY_SEQUENCE
 };
 
 STATIC_ASSERT(ARRAY_COUNT(sBackgroundMusicDefaultVolume) == SEQ_COUNT,
